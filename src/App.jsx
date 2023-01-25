d85a95708c08b93e6d9c4dd9bdfca74d704d2881
RyanWatts1 <devshop202@yahoo.com>
2023-01-25 12:37:17 -0500

    Hero section started

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,8 @@ const App = () => {
         </div>
       </div>
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          {" "}
-          <Hero />{" "}
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
         </div>
       </div>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
